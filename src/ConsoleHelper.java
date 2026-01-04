8875c9c5d56aef233d2eac0d400ebe16a80652d5
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-04 19:01:11 +1000

    added Operation and ConsoleHelper

---
+++ src/ConsoleHelper.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+
+    private static BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return bufReader.readLine();
+    }
+
+    public static int readInt() throws IOException {
+        String str = readString();
+        return Integer.parseInt(str.trim());
+    }
+}
