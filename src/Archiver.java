09ba0ac04796cecdd0c6087013da57194f8fb53f
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-04 13:45:41 +1000

    implemented the createZip method to handle file zipping

--- src/Archiver.java
+++ src/Archiver.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Type in the full path to zip the file.");
+        System.out.println("Enter the full path of the zip archive.");
         Scanner scanner = new Scanner(System.in);
         String stringToZip = scanner.nextLine();
         Path pathToZip = Paths.get(stringToZip);
         ZipFileManager zfm = new ZipFileManager(pathToZip);
 
-        System.out.println("Type in the full path to unzip the file.");
-        String stringToUnzip = scanner.nextLine();
-        Path pathToUnzip = Paths.get(stringToUnzip);
+        System.out.println("Enter the full path of the file to be zipped.");
+        String stringToZip2 = scanner.nextLine();
+        Path pathToUnzip = Paths.get(stringToZip2);
         zfm.createZip(pathToUnzip);
     }
 }
--- src/ZipFileManager.java
+++ src/ZipFileManager.java
@@ -1,4 +1,8 @@
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
 
@@ -9,6 +13,15 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
-
+        try (InputStream is = Files.newInputStream(source);
+             ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(zipFile));
+        ) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zos.putNextEntry(zipEntry);
+            while (is.available() > 0) {
+                zos.write(is.read());
+            }
+            zos.closeEntry();
+        }
     }
 }
