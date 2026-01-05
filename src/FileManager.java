4088caf48e69974f48e61e473247da8694ace8d4
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-05 12:29:37 +1000

    added FileManager to add a list of files to archive

---
+++ src/FileManager.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    private Path rootPath;
+    private List<Path> fileList;
+
+    public FileManager(Path rootPath) throws IOException {
+        this.rootPath = rootPath;
+        this.fileList = new ArrayList<>();
+        collectFileList(rootPath);
+    }
+
+    private void collectFileList(Path path) throws IOException {
+        if (Files.isRegularFile(path)) {
+            fileList.add(rootPath.relativize(path));
+        } else if (Files.isDirectory(path)){
+            try(DirectoryStream<Path> dirStream = Files.newDirectoryStream(path)) {
+                for (Path path1: dirStream) {
+                    collectFileList(path1);
+                }
+            }
+        }
+    }
+    public List<Path> getFileList() {
+        return fileList;
+    }
+}
